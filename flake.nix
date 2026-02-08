3a0365061ef454751489862895860efb6e997565
Nathan Gill <nathan.j.gill@outlook.com>
2026-02-08 21:11:02 +0000

    initial commit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A license file generator";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            ocaml
+            dune
+            ocamlPackages.utop
+            ocamlPackages.ocaml-lsp
+          ];
+        };
+      }
+    );
+}
